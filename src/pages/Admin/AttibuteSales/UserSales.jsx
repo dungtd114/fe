086c61774090bf021a66904a47f1ca6c495b9c53
dungtd114 <dungtdtph53575@gmail.com>
2025-08-18 16:49:39 +0700

    updateCode

--- src/pages/Admin/AttibuteSales/UserSales.jsx
+++ src/pages/Admin/AttibuteSales/UserSales.jsx
@@ -95,7 +95,6 @@ const Client = forwardRef(({
             setShowSearchInput(false);
         }
     }, [hoaDonId, khachHangMap]);
-
     useEffect(() => {
         // Load hình thức nhận hàng theo hoaDonId
         const savedHinhThucNhanHang = localStorage.getItem(`hinhThucNhanHang-${hoaDonId}`);
@@ -261,7 +260,6 @@ const Client = forwardRef(({
             setLoading(false);
         }
     };
-
     const handleReload = () => {
         setShowSearchInput(true);
         setSearchSdt('');
@@ -308,7 +306,7 @@ const Client = forwardRef(({
             }));
         }
 
-        console.log("Selected diaChiNhanId: ", dc.soDienThoai);
+
         localStorage.setItem(`diaChiNhanId-${hoaDonId}`, dc.id || index);
     };
 
@@ -319,7 +317,6 @@ const Client = forwardRef(({
         localStorage.setItem(`hinhThucNhanHang-${hoaDonId}`, hinhThucNhanHang);
         setDaXacNhanState(true);
     };
-
     const handleChangeNewDiaChi = (e) => {
         const { name, value } = e.target;
         setNewDiaChi(prev => ({ ...prev, [name]: value }));
@@ -336,11 +333,10 @@ const Client = forwardRef(({
         const tinh = provinces.find(p => p.code == selectedProvince)?.name || '';
         const huyen = districts.find(d => d.code == selectedDistrict)?.name || '';
         const xa = wards.find(w => w.code == selectedWard)?.name || '';
-        const diaChiDayDu = `${diaChi}, ${xa}, ${huyen}, ${tinh}`;
         const payload = {
             tenNguoiNhan: newDiaChi.tenNguoiNhan,
             soDienThoai: newDiaChi.soDienThoai,
-            diaChi: diaChiDayDu,
+            diaChi: diaChi,
             soNha: diaChi,
             tinh: tinh,
             huyen: huyen,
@@ -719,10 +715,10 @@ const Client = forwardRef(({
                         </button>
                     </div>
                 )}
-
+{/* 
                 {!khachHang && !showSearchInput && (
                     <p>Khách hàng: <strong>Khách lẻ</strong></p>
-                )}
+                )} */}
             </div>
 
             {showAddModal && (
